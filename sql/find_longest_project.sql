c5786daa24bea4d011ee740ae40a557998ccf42b
Andy <andrew.zerko@gmail.com>
2023-02-05 14:28:23 +0200

    Add project name to the query in find_longest_project.sql

--- sql/find_longest_project.sql
+++ sql/find_longest_project.sql
@@ -1,4 +1,4 @@
-SELECT id, TIMESTAMPDIFF(MONTH, start_date, finish_date) months_count
+SELECT id, name, TIMESTAMPDIFF(MONTH, start_date, finish_date) months_count
 FROM project
 WHERE TIMESTAMPDIFF(MONTH, start_date, finish_date) =
       (SELECT TIMESTAMPDIFF(MONTH, start_date, finish_date) months_count
